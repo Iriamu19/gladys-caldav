e977ff16a272d7291ace302de48e19395c62c7ec
jacquesh82 <jacques@hullu.fr>
2017-08-12 17:15:06 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 module.exports = function(sails) {
-
-	var getPlanning   = require('./lib/getPlanning.js');
-	var exec 		  		= require('./lib/exec.js');
-  var setup 		    = require('./lib/setup.js');
-	var config 		    = require('./lib/config.js');
-	var command		    = require('./lib/command.js');
-
-    return {
-        getPlanning,
-        exec,
-        setup,
-				config,
-				command
-    };
+  return {
+    getPlanning: require('./lib/getPlanning')
+  };
 };
